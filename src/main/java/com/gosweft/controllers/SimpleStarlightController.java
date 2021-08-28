fd09a3f79220598557d6693a811585aca3da732b
Nikhil Dahake <nikhildahake@hotmail.com>
2021-08-29 01:23:09 +0530

    Modify controller to accept json payload for input

--- src/main/java/com/gosweft/controllers/SimpleStarlightController.java
+++ src/main/java/com/gosweft/controllers/SimpleStarlightController.java
@@ -5,12 +5,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gosweft.helpers.EditDistance;
+import com.gosweft.helpers.EditDistanceInput;
 import com.gosweft.services.SweftService;
 
 /**
@@ -29,9 +30,9 @@ public class SimpleStarlightController {
 	
 	@RequestMapping(value = "/editdistance", method = RequestMethod.POST)
 	@ResponseBody
-	public EditDistance getEditDistance(@RequestParam("string1")String string1, @RequestParam("string2")String string2) {
+	public EditDistance getEditDistance(@RequestBody EditDistanceInput editDistanceInput) {
 		EditDistance ed = new EditDistance();
-		ed.setDistance(sweftService.getEditDistance(string1, string2));
+		ed.setDistance(sweftService.getEditDistance(editDistanceInput.getString1(), editDistanceInput.getString2()));
 		return ed;
 	}
 }
