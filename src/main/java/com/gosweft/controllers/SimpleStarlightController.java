e735a930bacbbfd03cf9818d05db8a13c2385498
Nikhil Dahake <nikhildahake@hotmail.com>
2021-08-29 02:17:50 +0530

    Allow cors

--- src/main/java/com/gosweft/controllers/SimpleStarlightController.java
+++ src/main/java/com/gosweft/controllers/SimpleStarlightController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,7 @@ public class SimpleStarlightController {
 		return "checkout";
 	}
 	
+	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "/editdistance", method = RequestMethod.POST)
 	@ResponseBody
 	public EditDistance getEditDistance(@RequestBody EditDistanceInput editDistanceInput) {
